4252dde200eee25ac912e574814602abba7ea07e
Pontus Östlund <ponost@roxen.com>
2021-04-12 15:43:50 +0200

    Initial code commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      >Frontend Mentor</a
+    >. Coded by <a href="https://github.com/poppa/">Pontus Östlund</a>.
+  </div>
+</main>
+
+<style>
+</style>
